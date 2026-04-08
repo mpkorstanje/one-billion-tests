e0baf4b49ac6edfd3611edcfb2ac4ce952f12615
M.P. Korstanje <rien.korstanje@gmail.com>
2026-04-08 02:20:21 +0200

    Add some diagnostics

--- src/test/java/com/example/LotsOfTests.java
+++ src/test/java/com/example/LotsOfTests.java
@@ -14,13 +14,17 @@ class LotsOfTests {
     private static final int ONE_MILLION =     1_000_000;
     private static final int ONE_BILLION = 1_000_000_000;
     private static final long LIMIT = ONE_BILLION;
-
+    private static long startTime;
     @TestFactory
     Stream<DynamicTest> test(){
+        startTime = System.currentTimeMillis();
         Stream<Long> longStream = LongStream.range(0, LIMIT).boxed();
         return DynamicTest.stream(longStream, String::valueOf, aLong -> {
-            if(aLong % 10_000 == 0) {
-                System.out.println(aLong);
+            if(aLong % 1_000_000 == 0) {
+                double testPerSecond = aLong.doubleValue() / (System.currentTimeMillis() - startTime) * 1000;
+                long testRemaining = LIMIT - aLong;
+                double minutesRemaining = testRemaining / testPerSecond / 60;
+                System.out.println(aLong + " " + testPerSecond + " test/s " + minutesRemaining + " min left");
             }
 
 //            // Show that we're leaving garbage behind.
