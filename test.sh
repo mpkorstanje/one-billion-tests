f9f96020456694d685d00b788dd2e2b2adf1dd77
M.P. Korstanje <rien.korstanje@gmail.com>
2026-04-17 16:51:18 +0200

    Limit memory

--- test.sh
+++ test.sh
@@ -3,6 +3,7 @@
 mvn clean test-compile dependency:copy-dependencies
 java \
   -XX:+UseCompactObjectHeaders -XX:+UseStringDeduplication \
+  -Xmx1G \
   --class-path target/dependency/*:target/test-classes \
   com.example.TestRunner
 #  org.junit.platform.console.ConsoleLauncher execute --select-package=com.example
